ba7c21f2e0162ef8f55ccd010fe47763e925d5cb
Claudia Salsini <salsiniclaudia@gmail.com>
2026-07-09 16:19:46 +0200

    aggiunto localStorage con watch per salvare dati nel browser e non perderli al refresh

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,25 @@
 <script setup>
 
-import { ref, computed } from "vue"
+import { ref, computed, watch } from "vue"
 
-const attivitagiornaliere = ref ([ 
+const STORAGE_KEY = "attivita-giornaliere"
+
+const datiSalvati = localStorage.getItem(STORAGE_KEY)
+
+const attivitagiornaliere = ref (
+  datiSalvati
+    ? JSON.parse(datiSalvati)
+  : [ 
   {id: 1, todo: "Fare la spesa", completata: false },
   {id: 2, todo: "Lavare il cane", completata: false },
   {id: 3, todo: "Spostare appuntamento", completata: false }
 ]);
+
+watch (attivitagiornaliere,
+  (nuovoValore) => { localStorage.setItem(STORAGE_KEY, JSON.stringify(nuovoValore)) },
+  { deep: true }
+)
+
 
 const attivitacompletate = computed(() => { 
   return attivitagiornaliere.value.filter(attivita => attivita.completata);
@@ -29,6 +42,7 @@ const attivitadacompletare = computed(() => {
       <input type="checkbox" v-model="attivita.completata" />
   
       <span>{{ attivita.todo }}</span> 
+    <!-- {{...}} interpolazione Mustache: x stampare un dato dentro i ltemplate-->
     </div>
 
     <span> </span>
@@ -36,7 +50,6 @@ const attivitadacompletare = computed(() => {
 
     <h2>Completate</h2>
      <div v-for= "attivita in attivitacompletate" :key="attivita.id">
-   
 
 
       <span>{{ attivita.todo }}</span> 
